000649dc65e16c50c9b533c6ef06c85466c96f15
Mounika <111635599+mounika7204@users.noreply.github.com>
2024-11-11 18:18:49 +0530

    Time: 976 ms (5.05%) | Memory: 279.1 MB (5.00%) - LeetSync

--- 744-network-delay-time/network-delay-time.cpp
+++ 744-network-delay-time/network-delay-time.cpp
@@ -1,42 +1,37 @@
 class Solution {
 public:
-    int networkDelayTime(vector<vector<int>>& times, int V, int k) {
-      if(V==1) return 0; int mintime = 0;
-//create adjacency list
-        vector<vector<int>> adj[V];
-        for(int i = 0 ;i<times.size();i ++){
-            vector<int> v;
-            v.push_back(times[i][1]-1); //add the vertex/node
-            v.push_back(times[i][2]); //add the time/edgeweight
-            adj[times[i][0]-1].push_back(v);   
-        }
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
-        vector<int> dist(V);
-        //mark every distance as infinite
-        for(int i = 0; i< V;i++) dist[i] = 1e+9;
-        dist[k-1] = 0;
-        pq.push({0,k-1});
-        while(!pq.empty()){
-            int dis = pq.top().first;
-            int node = pq.top().second;
-            pq.pop();
-            
-            for(auto it : adj[node]){
-                int edgeWeight = it[1];
-                int adjNode = it[0];
-                //check for an existing shorter path
-                if(dis + edgeWeight < dist[adjNode]){
-                    dist[adjNode] = dis + edgeWeight;
-                    pq.push({dist[adjNode], adjNode});
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        vector<int> dist(n+1, 1e8);
+        dist[k] = 0;
+        for(int i=1;i<=n-1;i++)
+        {
+            for(vector<int> edge: times)
+            {
+                int u = edge[0];
+                int v = edge[1];
+                int wt = edge[2];
+                if(dist[u] != 1e8 && dist[u] + wt < dist[v])
+                {
+                    dist[v] = dist[u] + wt;
                 }
             }
         }
-        for(int i = 0 ; i< V ; i++){
-//if node is not reachable
-            if(dist[i] == 1e+9) return -1;
-            mintime = max(dist[i], mintime);
+         for(vector<int> edge: times)
+            {
+                int u = edge[0];
+                int v = edge[1];
+                int wt = edge[2];
+                if(dist[u] != 1e8 && dist[u] + wt < dist[v])
+                {
+                   return -1;
+                }
+            }
+        int ans = 0;
+        for(int i=1;i<dist.size();i++)
+        {
+            if(dist[i] == 1e8) return -1;
+            ans = max(ans,dist[i]);
         }
-        return mintime;
-    
+        return ans;
     }
 };
